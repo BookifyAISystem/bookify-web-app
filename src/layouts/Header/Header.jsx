9a52f634c153de498a64a481c6876592093d2ef6
HoangAnh15062002 <hoanganhzt1506@gmail.com>
2025-01-18 23:40:07 +0700

    update

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/logo1.png'
 import './Header.css'
 import {
   AppBar,
