a3a8b6ff5d9fd32a6b8d3c6629b81023a8229550
Võ Huy Hoàng <145216389+ferr2764@users.noreply.github.com>
2025-02-12 20:34:29 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return {
-    plugins: [react()],
-    server: {
-      historyApiFallback: true, // Fix lỗi 404 khi F5 trên React Router
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return id.toString().split("node_modules/")[1].split("/")[0].toString();
-            }
-          },
-        },
-      },
-      chunkSizeWarningLimit: 2000, // Giảm cảnh báo kích thước file
-    },
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+})
